7d8068ce05bdc11fd5cc9472351af3fa795a39e0
deokgoo <kkddgg1001@gmail.com>
2021-08-17 18:27:26 +0900

    spring boot init

---
+++ src/test/java/com/tech/watergallery/WaterGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.watergallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
