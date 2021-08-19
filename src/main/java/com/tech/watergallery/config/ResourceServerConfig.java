403686b4bfd4c40314be12697852472192b3d817
deokgoo <kkddgg1001@gmail.com>
2021-08-19 23:02:12 +0900

    feature: circleci setting

--- src/main/java/com/tech/watergallery/config/ResourceServerConfig.java
+++ src/main/java/com/tech/watergallery/config/ResourceServerConfig.java
@@ -1,10 +1,8 @@
 package com.tech.watergallery.config;
 
-import lombok.Value;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -15,8 +13,8 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(authz -> authz
-                        .antMatchers(HttpMethod.GET, "/foos/**").hasAuthority("SCOPE_read")
-                        .antMatchers(HttpMethod.POST, "/foos").hasAuthority("SCOPE_write")
+                        .antMatchers("/gallery").permitAll()
+                        .antMatchers("/gallery/test").authenticated()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt());
     }
