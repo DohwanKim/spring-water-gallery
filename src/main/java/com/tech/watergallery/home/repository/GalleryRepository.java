59e5b7af3daac0cb23b763e25a255a5b65d60c27
Dohwan Kim <windboy2004@naver.com>
2021-11-11 15:49:32 +0900

    jang's answer

--- src/main/java/com/tech/watergallery/home/repository/GalleryRepository.java
+++ src/main/java/com/tech/watergallery/home/repository/GalleryRepository.java
@@ -18,4 +18,6 @@ public interface GalleryRepository extends JpaRepository<Gallery, Long> {
 
     // save가 동작할때 created_at, updated_at  default 로 NOW() 가 동작하길 원함
     // 이 서버의 시간이 아니라 db서버의 현제 시간을 사용하고 싶음
+    
+    // ㄴ 답변: mysql db default 값에다가 CURRENT_TIMESTAMP 넣으면 댐.
 }
